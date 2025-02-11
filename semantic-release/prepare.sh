702bca1db131c47c72487d5ff4e51ae3ce9fcbf9
TsonasIoannis <32147385+TsonasIoannis@users.noreply.github.com>
2025-02-11 00:56:54 +0000

    chore: try release

--- semantic-release/prepare.sh
+++ semantic-release/prepare.sh
@@ -1,2 +1,4 @@
 go mod tidy
-go build -o dist cmd/main.go
+GOOS=linux GOARCH=amd64 go build -o dist/personal-finance-tracker-linux cmd/main.go
+GOOS=darwin GOARCH=amd64 go build -o dist/personal-finance-tracker-mac cmd/main.go
+GOOS=windows GOARCH=amd64 go build -o dist/personal-finance-tracker.exe cmd/main.go
