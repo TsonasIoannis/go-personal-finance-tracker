4d09de52126082c3c69d45377ff118ecfb0f0eca
TsonasIoannis <32147385+TsonasIoannis@users.noreply.github.com>
2026-03-16 18:55:03 +0000

    chore: go_modules lint fixes

---
+++ doc.go
@@ -0,0 +1,3 @@
+// Package personalfinancetracker exists to provide a root package for module-level tooling.
+// The application entrypoint remains under cmd/main.go.
+package personalfinancetracker
